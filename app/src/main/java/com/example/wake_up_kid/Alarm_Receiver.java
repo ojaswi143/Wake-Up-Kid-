15ed8d1ded854e93e0025d9f51b91c25b848b55a
ojaswi143 <ojaswibhardwaj143@gmail.com>
2020-08-05 16:33:11 +0530

    Add my Files

---
+++ app/src/main/java/com/example/wake_up_kid/Alarm_Receiver.java
@@ -0,0 +1,30 @@
+package com.example.wake_up_kid;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class Alarm_Receiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Log.d("Tag :: ","inside alarm receiver");
+
+        // fetch the extra string from intent
+        String get_your_string = intent.getExtras().getString("extra");
+
+        String asd = intent.getExtras().getString("extra1");
+        Log.d("Tag :: ", asd);
+
+        // create an intent to the ringtone service
+        Intent service_intent = new Intent(context, RingTonePlayingService.class);
+
+        // pass the extra string from Main Activity to the Ring Tone Playing Service
+        service_intent.putExtra("extra", get_your_string);
+
+        // start the ringtone service
+        context.startService(service_intent);
+
+    }
+}
---
+++ app/src/main/java/com/example/wake_up_kid/DataModel.java
@@ -0,0 +1,15 @@
+package com.example.wake_up_kid;
+
+public class DataModel {
+
+    String time;
+
+    public DataModel(String time) {
+
+        this.time = time;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}
